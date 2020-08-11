08e821fb53be31e395d634a76e8e8ded61f9b514
Luis Cesar <lutzcc1@gmail.com>
2020-08-11 11:35:34 -0500

    check validity

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ class Block {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
-        this.previousHash= previousHash;
+        this.previousHash = previousHash;
         this.hash = this.calculateHash();
     }
 
@@ -33,10 +33,27 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length - 1; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) return false;
+            if (currentBlock.previousHash !== previousBlock.hash) return false;
+
+            return true;
+        }
+    }
 }
 
 let lcoin = new Blockchain();
-lcoin.addBlock(new Block(1, "today", {from: "me", to: "you", quantity: "tons"}));
-lcoin.addBlock(new Block(2, "tomorrow", {from: "mama", to: "vera", quantity: 5}));
+lcoin.addBlock(new Block(1, "today", { from: "me", to: "you", quantity: "tons" }));
+lcoin.addBlock(new Block(2, "tomorrow", { from: "mama", to: "vera", quantity: 5 }));
 
 console.log(JSON.stringify(lcoin, null, 2));
+
+console.log("valid?", lcoin.isChainValid());
+lcoin.chain[1].data.quantity = "not that much";
+console.log(JSON.stringify(lcoin, null, 2));
+console.log("valid?", lcoin.isChainValid());
